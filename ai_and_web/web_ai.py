23a785dd5337341f164064a748d43a6318709c85
julblanke <j_blan06@wwu.de>
2023-12-01 15:45:36 +0100

    ref: major refactor

--- ai_and_web/web_ai.py
+++
@@ -1,9 +0,0 @@
-from ai_and_web.utils.crawler import Crawler
-
-
-def run():
-    response = Crawler(url="https://vm009.rz.uos.de/crawl/index.html").crawl()
-    debug=True
-
-if __name__ == "__main__":
-    run()
